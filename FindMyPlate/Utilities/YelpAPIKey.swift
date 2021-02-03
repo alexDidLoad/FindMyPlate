2a30dabd5270cdc5cdca49c8b7a4d56d6d45a69f
Alexander Ha <Ha.alex.h@gmail.com>
2021-02-03 15:55:02 -0800

    add api key

---
+++ FindMyPlate/Utilities/YelpAPIKey.swift
@@ -0,0 +1,10 @@
+//
+//  YelpAPIKey.swift
+//  FindMyPlate
+//
+//  Created by Alexander Ha on 2/3/21.
+//
+
+import Foundation
+
+let myYelpAPIKey = "nWw_wNDWE2ePm7iYF7x6ovwGfZrOeL_Rxf1IKqRbm66cF2Og6D-vhMYOrqgCMS_DajR0eGwEetArAKQ6UTtcMnDOXoS1s96TIFUg4sV07QRaQBJYT3l_pNTUJR0BYHYx"
